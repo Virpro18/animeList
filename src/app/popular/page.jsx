dd9626e20a74f1bebccb924ed615ec6060a1314e
Virpro18 <sintawidodo032@gmail.com>
2024-10-14 21:03:27 +0900

    execption

--- src/app/popular/page.jsx
+++ src/app/popular/page.jsx
@@ -2,7 +2,7 @@
 
 import HeaderMenu from "@/components/utilities/HeaderMenu";
 import Pagination from "@/components/utilities/Pagination";
-import {api} from "@/libs/api";
+import { api } from "@/libs/api";
 import { useEffect, useState } from "react";
 import AnimeCard from "@/components/animeCard";
 
@@ -31,28 +31,25 @@ const popular = () => {
     fetchData();
   }, [page]);
   try {
-
     return (
       <>
-      <HeaderMenu title={`ANIME TERPOPULER #${page}`} />
-      <section>
-      <AnimeCard animeData={topAnime} />
-      </section>
-      <Pagination
-        pagging={handlePageChange}
-        pageNumber={page}
-        lastPage={topAnime.pagination?.last_visible_page}
+        <HeaderMenu title={`ANIME TERPOPULER #${page}`} />
+        <section>
+          <AnimeCard animeData={topAnime} />
+        </section>
+        <Pagination
+          pagging={handlePageChange}
+          pageNumber={page}
+          lastPage={topAnime.pagination?.last_visible_page}
         />
-    </>
-  );
-} catch (error) {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold text-red-500">
-        {error.message}
-      </h1>
-    </div>
-  );
+      </>
+    );
+  } catch (error) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-3xl font-bold text-red-500">{error.message}</h1>
+      </div>
+    );
+  }
 };
-
 export default popular;
